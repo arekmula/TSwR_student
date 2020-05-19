Fixed tau calculation

controllers/feedback_linearization_controller.py
@@ -14,6 +14,21 @@ class FeedbackLinearizationController(Controller):
         """
         q1, q2, q1_dot, q2_dot = x
         q1_d_dot, q2_d_dot = v
-        tau = self.model.M(x) * np.transpose(v) + self.model.C(x) * np.transpose([q1_dot, q2_dot])
+
+        # create q** vector from given v
+        q_d_dot = np.array([[q1_d_dot[0]],
+                            [q2_d_dot[0]]])
+
+        # create q* vector from given x
+        q_dot = np.array([[q1_dot],
+                      [q2_dot]])
+
+        # create output vector
+        tau = np.zeros((2, 1), float)
+
+        # calculate tau from equation no 20
+        M_v = np.dot(self.model.M(x), q_d_dot)
+        C_q = np.dot(self.model.C(x), q_dot)
+        tau = M_v + C_q
 
         return tau

models/manipulator_model.py
@@ -1,14 +1,14 @@
 import numpy as np
 
+
 class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
-        self.Tp = Tp
         self.l1 = 0.5
-        self.r1 = 0.01
+        self.r1 = 0.25
         self.m1 = 1.
         self.l2 = 0.5
-        self.r2 = 0.01
+        self.r2 = 0.25
         self.m2 = 1.
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1 ** 2 + self.l1 ** 2)
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2 ** 2 + self.l2 ** 2)
@@ -16,9 +16,14 @@ class ManiuplatorModel:
         self.r3 = 0.0  # 0.01 before
         self.I_3 = 2. / 5 * self.m3 * self.r3 ** 2
 
-        self.alpha = self.m1 * self.r1 ** 2 + self.I_1 + self.m2 * (self.l1 ** 2 + self.r2 ** 2) + self.I_2
+        # calculate substitutions: alpha, beta and gamma from equation no (14)
+        self.alpha = ((self.m1 * pow(self.r1,2)) +
+                      self.I_1 +
+                      self.m2 * (pow(self.l1, 2) + pow(self.r2, 2))+
+                      self.I_2)
+
         self.beta = self.m2 * self.l1 * self.r2
-        self.gamma = self.m2 * self.r2 ** 2 + self.I_2
+        self.gamma = (self.m2 * pow(self.r2, 2)) + self.I_2
 
     def M(self, x):
         """
@@ -27,8 +32,9 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        M = np.arange(4).reshape(2,2) # create 2x2 array
-        # fill matrix with values
+        M = np.zeros((2, 2), float)  # create 2x2 array
+
+        # fill matrix with values from equation no 20
         M[0, 0] = self.alpha + 2 * self.beta * np.cos(q2)
         M[0, 1] = self.gamma + self.beta * np.cos(q2)
         M[1, 0] = self.gamma + self.beta * np.cos(q2)
@@ -43,12 +49,12 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        C = np.arange(4).reshape(2,2) # create 2x2 array
+        C = np.zeros((2, 2), float)
 
-        # fill matrix with values
-        C[0, 0] = -self.beta * np.sin(q2) * q2_dot
-        C[0, 1] = -self.beta * np.sin(q2) * (q1_dot + q2_dot)
-        C[1, 0] = self.beta * np.sin(q2) * q1_dot
-        C[1, 1] = 0
+        # fill matrix with values from equation no 20
+        C[0][0] = -self.beta * np.sin(q2) * q2_dot
+        C[0][1] = -self.beta * np.sin(q2) * (q1_dot + q2_dot)
+        C[1][0] = self.beta * np.sin(q2) * q1_dot
+        C[1][1] = 0
 
         return C